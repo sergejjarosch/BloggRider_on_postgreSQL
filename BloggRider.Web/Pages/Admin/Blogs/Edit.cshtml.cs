53286bdd0c1b2ab89e6ab86fc6f6876ccf03ba59
sergejjarosch <sergejjarosch@gmail.com>
2024-07-03 09:35:35 +0200

    Add - Delate function

--- BloggRider.Web/Pages/Admin/Blogs/Edit.cshtml.cs
+++ BloggRider.Web/Pages/Admin/Blogs/Edit.cshtml.cs
@@ -19,4 +19,40 @@ public class Edit : PageModel
     {
         BlogPost = _bloggRiderDbContext.BlogPosts.Find(id);
     }
+
+    public IActionResult OnPostEdit()
+    {
+        var existingBlogPost = _bloggRiderDbContext.BlogPosts.Find(BlogPost.Id);
+
+        if (existingBlogPost != null)
+        {
+            existingBlogPost.Heading = BlogPost.Heading;
+            existingBlogPost.PageTitle = BlogPost.PageTitle;
+            existingBlogPost.Content = BlogPost.Content;
+            existingBlogPost.ShortDescription = BlogPost.ShortDescription;
+            existingBlogPost.FeaturedImageUrl = BlogPost.FeaturedImageUrl;
+            existingBlogPost.UrlHandle = BlogPost.UrlHandle;
+            existingBlogPost.PublishedDate = BlogPost.PublishedDate;
+            existingBlogPost.Author = BlogPost.Author;
+            existingBlogPost.Visible = BlogPost.Visible;
+        }
+
+        _bloggRiderDbContext.SaveChanges();
+        return RedirectToPage("/Admin/Blogs/List");
+    }
+
+    public IActionResult OnPostDelete()
+    {
+      var existingBlog = _bloggRiderDbContext.BlogPosts.Find(BlogPost.Id);
+
+      if (existingBlog != null)
+      {
+          _bloggRiderDbContext.BlogPosts.Remove(existingBlog);
+          _bloggRiderDbContext.SaveChanges();
+
+          return RedirectToPage("/Admin/Blogs/List");
+      }
+
+      return Page(); // ToDo Message on screen - this BlogPost was not found
+    }
 }
